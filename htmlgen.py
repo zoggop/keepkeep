007656a51a6965ad00ff2150f5019edf3c3eabed
zoggop <zoggop@gmail.com>
2022-07-12 08:22:37 -0700

    add color chart

--- htmlgen.py
+++ htmlgen.py
@@ -4,12 +4,15 @@ import sqlite3
 import json
 from shutil import copy2
 from datetime import datetime
+from math import ceil
 
 basePath = os.path.expanduser('~/keepkeep')
 dbFilepath = basePath + '/keepkeep.db'
 takeoutPath = os.path.expanduser('~/Documents/takeout/Keep')
 
 imgExts = ['.jpg', '.png']
+
+colorOrder = ['DEFAULT', 'RED', 'ORANGE', 'YELLOW', 'GREEN', 'TEAL', 'BLUE', 'CERULEAN', 'PURPLE', 'PINK', 'BROWN', 'GRAY']
 
 conn = sqlite3.connect(dbFilepath)
 
@@ -71,8 +74,11 @@ def generateMonthPage(yearMonth, prevYearMonth, nextYearMonth):
 	rows = fetchMonth(yearMonth)
 	contentHtml = ''
 	wordCount, noteCount = 0, 0
+	colorCount = {}
+	colorWords = {}
 	for r in rows:
 		noteHtml = noteTempl
+		words = 0
 		for ci in range(0, len(columns)):
 			c = columns[ci]
 			v = r[ci]
@@ -82,7 +88,8 @@ def generateMonthPage(yearMonth, prevYearMonth, nextYearMonth):
 				dt = datetime.strptime(v, "%Y-%m-%d %H:%M:%S")
 				v = dt.strftime("%A, %B %#d, %Y, %#I:%M %p")
 			elif c == 'TEXTCONTENT':
-				wordCount += len(v.split())
+				words = len(v.split())
+				wordCount += words
 				v = paragrapher(v)
 			elif c == 'LISTCONTENT':
 				v = loadChecklist(v)
@@ -90,7 +97,11 @@ def generateMonthPage(yearMonth, prevYearMonth, nextYearMonth):
 				v = loadAttachments(v)
 			elif c == 'ANNOTATIONS':
 				v = loadAnnotations(v)
+			elif c == 'COLOR':
+				color = v
 			noteHtml = noteHtml.replace('%{}%'.format(c), str(v))
+		colorCount[color] = (colorCount.get(color) or 0) + 1
+		colorWords[color] = (colorWords.get(color) or 0) + words
 		contentHtml += noteHtml + '\n\n'
 		noteCount += 1
 	pageHtml = pageTempl
@@ -104,7 +115,7 @@ def generateMonthPage(yearMonth, prevYearMonth, nextYearMonth):
 		pageHtml = pageHtml.replace('%NEXTDATE%', yearMonthFormat(nextYearMonth))
 	with open('{}/{}.html'.format(basePath, yearMonth), 'w', encoding='utf-8') as write_file:
 		write_file.write(pageHtml)
-	return { 'words': wordCount, 'notes': noteCount }
+	return { 'words': wordCount, 'notes': noteCount, 'colors': colorWords }
 
 def generateOverviewPage(years, stats):
 	contHtml = '<table class="overviewtable">\n'
@@ -113,7 +124,16 @@ def generateOverviewPage(years, stats):
 		for ym in years[y]:
 			dt = datetime.strptime(ym, "%Y-%m")
 			mname = dt.strftime("%B")
-			contHtml += '<tr class="overviewmonth">\n<td><h3><a href="{}.html">{}</a></h3></td>\n<td>{:,} words</td>\n<td>{:,} notes</td>\n</tr>\n'.format(ym, mname, stats.get(ym).get('words'), stats.get(ym).get('notes'))
+			colorCount = stats.get(ym).get('colors')
+			maxCount = colorCount.get('max')
+			colorChart = '<table class="colorchart"><tr>'
+			for color in colorOrder:
+				count = colorCount.get(color)
+				if not count is None:
+					blocks = ceil((count / stats.get(ym).get('words')) * 50)
+					colorChart += '<td class="count {}">{}</td>'.format(color, '-' * blocks)
+			colorChart += '</tr></table>'
+			contHtml += '<tr class="overviewmonth">\n<td><h3><a href="{}.html">{}</a></h3></td>\n<td>{:,} words</td>\n<td>{:,} notes</td>\n<td>{}</td>\n</tr>\n'.format(ym, mname, stats.get(ym).get('words'), stats.get(ym).get('notes'), colorChart)
 	contHtml += '</table>'
 	html = overTempl
 	html = html.replace('%CONTENT%', contHtml)
